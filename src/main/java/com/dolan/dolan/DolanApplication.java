3c270729e20874bd9c01a7f676f4d7c411d16b2b
Ahmad Asroruddin <asroruddinahmad09@gmail.com>
2024-05-17 19:19:58 +0700

    initial

---
+++ src/main/java/com/dolan/dolan/DolanApplication.java
@@ -0,0 +1,13 @@
+package com.dolan.dolan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DolanApplication.class, args);
+    }
+
+}
